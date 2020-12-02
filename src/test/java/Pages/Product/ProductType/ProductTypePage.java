80479bf946250650d7dd807aa1e250423d527e0c
sucilestari <slestari@alterra.id>
2020-12-02 20:16:31 +0700

    All Scenario for Product Type Name Done

--- src/test/java/Pages/Product/ProductType/ProductTypePage.java
+++ src/test/java/Pages/Product/ProductType/ProductTypePage.java
@@ -1,11 +1,16 @@
 package Pages.Product.ProductType;
 
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.thucydides.core.pages.PageObject;
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class ProductTypePage extends PageObject {
+    String names;
+
     @FindBy(id = "qe_product")
     WebElement PRODUCT_MENU;
 
@@ -21,6 +26,25 @@ public class ProductTypePage extends PageObject {
     @FindBy(className = "qe_product-type_product-type-name_table_header")
     WebElement PRODUCT_TYPE_NAME_HEADER;
 
+    @FindBy(xpath = "//span[contains(text(),'»')]")
+    WebElement LAST_PAGE_DATA;
+
+    @FindBy(xpath = "//button[contains(text(),'Update')]")
+    WebElement UPDATE_BUTTON;
+
+    @FindBy(xpath = "//button[contains(text(),'Ok')]")
+    WebElement OK_BUTTON;
+
+    @FindBy(id = "__BVID__326")
+    WebElement NAME_UPDATE;
+
+    public WebElement getDataTable(int index){
+        WebElement mytable = getDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[1]/div[1]/div[1]/table[1]"));
+        List<WebElement> rows_table = mytable.findElements(By.tagName("tr"));
+        List <WebElement> Columns_row = rows_table.get(rows_table.size()-1).findElements(By.tagName("td"));
+        return Columns_row.get(index);
+    }
+
     public void OnProductMenu()throws InterruptedException{
         PRODUCT_MENU.click();
         Thread.sleep(3000);
@@ -35,7 +59,25 @@ public class ProductTypePage extends PageObject {
         Assert.assertTrue(PRODUCT_TYPE_NAME_HEADER.isDisplayed());
     }
 
-    public void GetCorrectHeader(String header){
-        Assert.assertEquals(header,PRODUCT_TYPE_ID_HEADER.getText());
+    public void clickEditButton(){
+        LAST_PAGE_DATA.click();
+        getDataTable(3).findElements(By.tagName("button")).get(0).click();
+    }
+
+    public void changeName(String name){
+        names = name;
+        NAME_UPDATE.clear();
+        NAME_UPDATE.sendKeys(name);
+        UPDATE_BUTTON.click();
+        OK_BUTTON.click();
+    }
+
+    public void GetCorrectHeader(){
+        Assert.assertEquals(names,getDataTable(2).getText());
+        getDataTable(3).findElements(By.tagName("button")).get(0).click();
+        NAME_UPDATE.clear();
+        UPDATE_BUTTON.click();
+        OK_BUTTON.click();
+        Assert.assertEquals("",getDataTable(2).getText());
     }
 }
--- src/test/resources/features/Product/ProductType/ProductType.feature
+++ src/test/resources/features/Product/ProductType/ProductType.feature
@@ -1,14 +1,20 @@
+@Product
 Feature: Product Type
 As Ops
 I want to have rename Poduct type id to Product Type ID
 So that header name looks better
 
-  Background: Login
+  Background: Login Success (As Admin)
     Given User open sentinel login page
-    When User input febri as username and input password
+    When User input kraken as username and input password
+    Then User can see sentinel dashboard
 
-  @TestCaseKey=BPAS-T356
-  Scenario: correct header name of "product type" not "poduct type" anymore
+  @TestCaseKey=BPAS-T3030
+  Scenario Outline: Update Product Type Name Success
     Given on product menu
     When user click product type sub menu
-    Then user get correct header, that is Product Type ID
+    And Click Edit on data that want to update and change name to <name>
+    Then Data already updated
+    Examples:
+    |name     |
+    |ayokayok |
